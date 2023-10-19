9d676fe41b13d841fe02b8ca29ad92af769e48a5
unknown <elbiasimone@hotmail.com>
2023-10-19 00:41:11 -0300

    mapeamento ORM

---
+++ src/main/java/me/elbia/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package me.elbia.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/elbia/domain/model/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.elbia.domain.model.repository;
+
+import me.elbia.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
